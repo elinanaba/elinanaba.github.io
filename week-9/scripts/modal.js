74e76d13fcc413ac089ace7d56b19e219a63457d
Elina Sohn <elina@Elinas-MacBook-Air.local>
2019-11-26 23:44:49 -0800

    yayyyyy multi modal

--- week-9/scripts/modal.js
+++ week-9/scripts/modal.js
@@ -1,25 +1,19 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+var btn = document.getElementsByClassName("click-to-open");
+var modal = document.getElementsByClassName('modal-window');
+var span = document.getElementsByClassName("close");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+for (var i = 0; i < btn.length; i++) {
+    var thisBtn = btn[i];
+    thisBtn.addEventListener("click", function(){
+      var modal = document.getElementById(this.dataset.modal);
+      modal.style.display = "block";
+  }, false);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+for (var i = 0; i < modal.length; i++) {
+    modal[i].addEventListener("click", function(e) {
+      if(e.target.className === 'close') {
+        this.style.display = 'none';
+      }
+    }, false);
 }
